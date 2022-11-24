ca5e5f5dc9ef9e3cadd10a3a937763dd629ccaa6
Jonathan FM <jonathanfernandezfm@gmail.com>
2022-11-24 17:31:48 +0200

    fix nombre de guild

--- interactions/formulario_modal_submit.js
+++ interactions/formulario_modal_submit.js
@@ -44,7 +44,7 @@ module.exports = {
 			.addFields(
 				{ name: '1) Nombre de usuario en Albion:', value: `> ${nombre}` },
 				{ name: '2) ¿Resides en España?:', value: `> ${horario}` },
-				{ name: '3) ¿Qué contenido quieres hacer en Kymera?:', value: `> ${contenido}` },
+				{ name: '3) ¿Qué contenido quieres hacer en MistTake?:', value: `> ${contenido}` },
 				{ name: '4) ¿Cuál es tu anterior gremio?:', value: `> ${guild}` },
 				{ name: '5) Rol principal, secundario y armas usadas/spec:', value: `> ${rol}` },
 				{ name: 'Fama estimada PvP + PvE', value: `> ${(totalFame / 10 ** 6).toFixed(2)} M` }
